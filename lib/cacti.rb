c8b2cd4fe521be9545c7b8bfb79489f84e705f52
renatoornelas <renato@openx.com.br>
2014-07-16 20:46:26 -0300

    revisao do lib/cacti.rb para evitar varias requisicoes

--- lib/cacti.rb
+++ lib/cacti.rb
@@ -1,43 +1,48 @@
-require "net/http"
-require "JSON"
+require "net/https"
+require "uri"
+require "json"
 require "date"
 
 class Cacti
-    attr_reader :url, :port
+    attr_reader :uri, :data_sources, :graph_data
     # Pass in the url to where you cacti instance is hosted
-    def initialize(url, port = 80)
-        @url = url
-        @port = port
+    def initialize(url, graph_id, data_sources=["IN ", "OUT"], graph_start=nil, graph_end=nil)
+        @uri = URI.parse(url)
+        @graph_id = graph_id
+        @data_sources = data_sources
+ 
+        graph_start ||= Time.new.to_i - 86400;
+        
+        @graph_data = query graph_id, graph_start, graph_end
     end
 
+    # This is the raw query method, it will fetch the 
+    # JSON from the Graphite and parse it
+    def query(graph_id, graph_start=nil, graph_end=nil)
+        graph_start ||= Time.new.to_i - 86400;
+        
+        http     = Net::HTTP.new (@uri.host)
+        
+        http.use_ssl = (@uri.port == 443)
+        
+        request  = http.request Net::HTTP::Get.new("#{@uri.request_uri}?local_graph_id=#{graph_id}&rra_id=0&view_type=&graph_start=#{graph_start}&graph_end=#{graph_end}")
+
+        result = JSON.parse(request.body, :symbolize_names => true, :allow_nan => true)
+        return result
+    end
+    
     def get_value(datapoint,index)
         value = datapoint[index] || 0
         return value.round(2)
     end
     
-    # This is the raw query method, it will fetch the 
-    # JSON from the Graphite and parse it
-    def query(graph_id, graph_start=nil, graph_end=nil)
-        graph_start ||= Time.new.to_i - 86400;
-        #graph_end ||= Time.new.to_i;
-        
-        http     = Net::HTTP.new "#{@url}"
-        
-        request  = http.request Net::HTTP::Get.new("/graph_json.php?local_graph_id=#{graph_id}&rra_id=0&view_type=&graph_start=#{graph_start}&graph_end=#{graph_end}")
-
-        result = JSON.parse(request.body, :symbolize_names => true, :allow_nan => true)
-
-        return result
-    end
-
     # This is high-level function that will fetch a set of datapoints
     # since the given start point and convert it into a format that the
     # graph widget of Dashing can understand
-    def points(graph_id, data_source, graph_start=nil, graph_end=nil)
-        stats = query graph_id, graph_start, graph_end
-        datapoints = stats[:data]
+    def points(data_source)
+        datapoints = @graph_data[:data]
 
-        index = stats[:meta][:legend].find_index(data_source)
+        index = @graph_data[:meta][:legend].find_index(data_source)
         points = []
 
         (datapoints.select { |el| not el[0].nil? }).each do|item|
@@ -47,24 +52,36 @@ class Cacti
 
         return points
     end
+    
+    def series
+      series = []
+      @data_sources.each do |ds|
+        series << points(ds)
+      end
+      return series
+    end
 
     # Not all Dashing widgets need a set of points, often just 
     # the current value is enough. This method does just that, it fetches
     # the value for last point-in-time and returns it
-    def value(graph_id, data_source, graph_start=nil, graph_end=nil)
-        stats = query graph_id, graph_start, graph_end
+    def value(data_source)
         
-        index = stats[:meta][:legend].find_index(data_source)
+        index = @graph_data[:meta][:legend].find_index(data_source)
         
-        last = (stats[:data].select { |el| not el[0].nil? }).last
-        
+        last = (@graph_data[:data].select { |el| not el[0].nil? }).last
         return get_value(last,index)
     end
     
-    def title(graph_id)
-        stats = query graph_id
-        title = stats[:meta][:title]
-        return title
+    def values
+      values = []
+      @data_sources.each do |ds|
+        values << value(ds)
+      end
+      return values
+    end
+      
+    def title
+        title = @graph_data[:meta][:title]
     end
     
 end
